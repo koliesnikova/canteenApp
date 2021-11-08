8cfcbdf618ae9bd62d8850b8080b483140705c0a
Zuzana <Zuzana@DESKTOP-5PH8R4I>
2021-11-08 19:41:28 +0100

    edited with amountAvailiable variable

--- src/main/java/sk/upjs/paz1c/main/MysqlIngredientDao.java
+++ src/main/java/sk/upjs/paz1c/main/MysqlIngredientDao.java
@@ -11,16 +11,16 @@ import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 
 public class MysqlIngredientDao implements IngredientDao {
-	
+
 	JdbcTemplate jdbcTemplate;
-	
+
 	public MysqlIngredientDao(JdbcTemplate jdbcTemplate) {
 		this.jdbcTemplate = jdbcTemplate;
 	}
 
 	@Override
 	public List<Ingredient> getAll() {
-		String sql = "SELECT id, name, price, amount FROM ingredient";
+		String sql = "SELECT id, name, price, amount, amount_availiable FROM ingredient";
 		return jdbcTemplate.query(sql, new RowMapper<>() {
 
 			@Override
@@ -29,7 +29,8 @@ public class MysqlIngredientDao implements IngredientDao {
 				String name = rs.getString("name");
 				double price = rs.getDouble("price");
 				String amount = rs.getString("amount");
-				return new Ingredient(id, name, price, amount);
+				String amountAvailiable = rs.getString("amount_availiable");
+				return new Ingredient(id, name, price, amount, amountAvailiable);
 			}
 		});
 	}
@@ -40,19 +41,18 @@ public class MysqlIngredientDao implements IngredientDao {
 			SimpleJdbcInsert insert = new SimpleJdbcInsert(jdbcTemplate);
 			insert.withTableName("ingredient");
 			insert.usingGeneratedKeyColumns("id");
-			insert.usingColumns("name", "price", "amount");
-			
+			insert.usingColumns("name", "price", "amount", "amount_availiable");
+
 			Map<String, Object> values = new HashMap<>();
 			values.put("name", ingredient.getName());
 			values.put("price", ingredient.getPrice());
 			values.put("amount", ingredient.getAmount());
-			return new Ingredient(insert.executeAndReturnKey(values).longValue(),
-					ingredient.getName(),
-					ingredient.getPrice(),
-					ingredient.getAmount());
+			return new Ingredient(insert.executeAndReturnKey(values).longValue(), ingredient.getName(),
+					ingredient.getPrice(), ingredient.getAmount(), ingredient.getAmountAvailiable());
 		} else { // update
-			String sql = "UPDATE ingredient SET name = ?, price = ?, amount = ? WHERE id = ?";
-			int changedCount = jdbcTemplate.update(sql, ingredient.getName(), ingredient.getPrice(), ingredient.getAmount());
+			String sql = "UPDATE ingredient SET name = ?, price = ?, amount = ?, amount_availiable = ? WHERE id = ?";
+			int changedCount = jdbcTemplate.update(sql, ingredient.getName(), ingredient.getPrice(),
+					ingredient.getAmount(), ingredient.getAmountAvailiable());
 			if (changedCount == 1)
 				return ingredient;
 			else
