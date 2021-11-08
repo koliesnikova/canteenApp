7fd8e28170c506923fb093be79fb306cb90d08d9
Zuzana <Zuzana@DESKTOP-5PH8R4I>
2021-11-08 21:33:56 +0100

    added getIngredientDao

--- src/main/java/sk/upjs/paz1c/main/DaoFactory.java
+++ src/main/java/sk/upjs/paz1c/main/DaoFactory.java
@@ -1,24 +1,36 @@
 package sk.upjs.paz1c.main;
 
-
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import com.mysql.cj.jdbc.MysqlDataSource;
 
 public enum DaoFactory {
-	
+
 	INSTANCE;
-	
+
 	private JdbcTemplate jdbcTemplate;
+	private boolean testing = false;
+	private IngredientDao ingredientDao;
 	
-	public JdbcTemplate getjdJdbcTemplate() {
+
+	public void testing() {
+		testing = true;
+	}
+	
+	public IngredientDao getIngredientDao() {
+		if (ingredientDao == null) {
+			ingredientDao = new MysqlIngredientDao(getJdbcTemplate());
+		}
+		return ingredientDao;
+	}
+
+	public JdbcTemplate getJdbcTemplate() {
 		if (jdbcTemplate == null) {
 			MysqlDataSource dataSource = new MysqlDataSource();
 			dataSource.setUser("canteen");
 			dataSource.setPassword("canteenApp2021");
-			dataSource.setUrl("jdbc:mysql://localhost:3306/projekt_jedalen?"
-					+ "serverTimezone=Europe/Bratislava");
-			
+			dataSource.setUrl("jdbc:mysql://localhost:3306/projekt_jedalen?" + "serverTimezone=Europe/Bratislava");
+
 			jdbcTemplate = new JdbcTemplate(dataSource);
 		}
 		return jdbcTemplate;
