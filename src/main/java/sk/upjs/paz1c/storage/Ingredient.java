23c26e9101066e5aed9621d801ae2cd975e5a9c5
Zuzana <Zuzana@DESKTOP-5PH8R4I>
2021-11-09 19:28:52 +0100

    pridana metoda equals

--- src/main/java/sk/upjs/paz1c/storage/Ingredient.java
+++ src/main/java/sk/upjs/paz1c/storage/Ingredient.java
@@ -70,6 +70,59 @@ public class Ingredient {
 		return "Ingredient [id=" + id + ", name=" + name + ", price=" + price + ", amount=" + amount
 				+ ", amountAvailiable=" + amountAvailiable + "]";
 	}
+
+
+//	@Override
+//	public int hashCode() {
+//		final int prime = 31;
+//		int result = 1;
+//		result = prime * result + ((amount == null) ? 0 : amount.hashCode());
+//		result = prime * result + ((amountAvailiable == null) ? 0 : amountAvailiable.hashCode());
+//		result = prime * result + ((id == null) ? 0 : id.hashCode());
+//		result = prime * result + ((name == null) ? 0 : name.hashCode());
+//		result = prime * result + ((price == null) ? 0 : price.hashCode());
+//		return result;
+//	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Ingredient other = (Ingredient) obj;
+		if (amount == null) {
+			if (other.amount != null)
+				return false;
+		} else if (!amount.equals(other.amount))
+			return false;
+		if (amountAvailiable == null) {
+			if (other.amountAvailiable != null)
+				return false;
+		} else if (!amountAvailiable.equals(other.amountAvailiable))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (price == null) {
+			if (other.price != null)
+				return false;
+		} else if (!price.equals(other.price))
+			return false;
+		return true;
+	}
+	
+	
 	
 	
 }
