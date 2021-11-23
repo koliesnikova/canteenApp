d0b8fc0f25558cc165202eef8120aaf5a14d2c8d
Šimon <imon@DESKTOP-RG72TU9>
2021-11-23 19:51:17 +0100

    modified createIngredientScene.fxml, createIngredientController

--- src/main/java/sk/upjs/paz1c/main/CreateIngredientSceneController.java
+++ src/main/java/sk/upjs/paz1c/main/CreateIngredientSceneController.java
@@ -2,14 +2,51 @@ package sk.upjs.paz1c.main;
 
 import sk.upjs.paz1c.storage.Ingredient;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.TextField;
+
 public class CreateIngredientSceneController {
+	@FXML
+	private Spinner<?> amountAvailableSpinner;
+
+	@FXML
+	private TextField nameTextField;
+
+	@FXML
+	private TextField priceTextField;
+
+	@FXML
+	private ComboBox<?> standardAmountComboBox;
+
+	@FXML
+	private TextField standardAmountTextField;
+
 	
 	public CreateIngredientSceneController(Ingredient ingredient) {
 		// TODO Auto-generated constructor stub
 	}
-	
+
 	public CreateIngredientSceneController() {
 		// TODO Auto-generated constructor stub
+	}
+	
+
+	@FXML
+	void initialize() {
+
+	}
+
+	@FXML
+	void cancelChanges(ActionEvent event) {
+
+	}
+
+	@FXML
+	void saveChanges(ActionEvent event) {
+
 	}
 
 }
