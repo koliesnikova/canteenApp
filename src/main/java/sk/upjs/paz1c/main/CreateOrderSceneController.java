9fb4db5cff79f2ac99f1014f3a7316eaffff0d10
Šimon <imon@DESKTOP-RG72TU9>
2021-11-25 22:16:21 +0100

    added OrderFxModel

--- src/main/java/sk/upjs/paz1c/main/CreateOrderSceneController.java
+++ src/main/java/sk/upjs/paz1c/main/CreateOrderSceneController.java
@@ -1,15 +1,67 @@
 package sk.upjs.paz1c.main;
 
+import sk.upjs.paz1c.storage.Food;
 import sk.upjs.paz1c.storage.Order;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.TableView;
+
 public class CreateOrderSceneController {
+	
+	@FXML
+    private Button addButton;
+
+    @FXML
+    private DatePicker dayDatePicker;
+
+    @FXML
+    private Button deleteButton;
+
+    @FXML
+    private ComboBox<Food> foodComboBox;
+
+    @FXML
+    private Spinner<Integer> portionsSpinner;
+
+    @FXML
+    private TableView<?> portionsTableView;
 
 	public CreateOrderSceneController(Order selectedOrder) {
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	public CreateOrderSceneController() {
-		// TODO Auto-generated constructor stub
+		
 	}
 
+    @FXML
+    void initialize() {
+
+    }
+    
+    @FXML
+    void addFood(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cancelChanges(ActionEvent event) {
+    	foodComboBox.getScene().getWindow().hide();
+    }
+
+    @FXML
+    void deleteFood(ActionEvent event) {
+
+    }
+
+    @FXML
+    void saveChanges(ActionEvent event) {
+
+    }
+	
 }
