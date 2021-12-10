8307b045f95ce8c82435f60fea45af3e707929af
Zuzana <Zuzana@DESKTOP-5PH8R4I>
2021-12-10 20:25:57 +0100

    two tests implemented

--- src/main/java/sk/upjs/paz1c/biznis/CanteenManager.java
+++ src/main/java/sk/upjs/paz1c/biznis/CanteenManager.java
@@ -1,12 +1,17 @@
 package sk.upjs.paz1c.biznis;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import sk.upjs.paz1c.storage.Food;
-import sk.upjs.paz1c.storage.Order;
 
 public interface CanteenManager {
 	
 	List<Food> filterFoodNotInOrder(Long orderId);
-
+	
+	List<ShoppingListItemOverview> getAllToBuy();
+	
+	int getNumberOfToBuy();
+	
+	 List<ShoppingListItemOverview> getItemsForShoppingList(LocalDateTime date);
 }
--- src/test/java/sk/upjs/paz1c/biznis/DefaultCanteenManagerTest.java
+++ src/test/java/sk/upjs/paz1c/biznis/DefaultCanteenManagerTest.java
@@ -16,6 +16,8 @@ import sk.upjs.paz1c.storage.DaoFactory;
 import sk.upjs.paz1c.storage.EntityUndeletableException;
 import sk.upjs.paz1c.storage.Food;
 import sk.upjs.paz1c.storage.FoodDao;
+import sk.upjs.paz1c.storage.Ingredient;
+import sk.upjs.paz1c.storage.IngredientDao;
 import sk.upjs.paz1c.storage.Order;
 import sk.upjs.paz1c.storage.OrderDao;
 
@@ -23,21 +25,27 @@ class DefaultCanteenManagerTest {
 	
 	private OrderDao orderDao;
 	private FoodDao foodDao;
+	private IngredientDao ingredientDao;
 	private Order savedOrder;
 	private Food savedFood1;
 	private Food savedFood2;
+	private Ingredient savedIngredient;
 	private CanteenManager canteenManager;
 	
 	public DefaultCanteenManagerTest() {
 		DaoFactory.INSTANCE.testing();
 		orderDao = DaoFactory.INSTANCE.getOrderDao();
 		foodDao = DaoFactory.INSTANCE.getFoodDao();
+		ingredientDao = DaoFactory.INSTANCE.getIngredientDao();
 		canteenManager = new DefaultCanteenManager();
 	}
 
 	@BeforeEach
 	void setUp() throws Exception {
-		savedFood1 = foodDao.save(new Food("testCanteenManager1"));
+		savedIngredient = ingredientDao.save(new Ingredient("test ingred. ", 5.5, "100 ml", 0));
+		HashMap<Ingredient, Integer> ingredientsInFood = new HashMap<Ingredient, Integer>();
+		ingredientsInFood.put(savedIngredient, 2);
+		savedFood1 = foodDao.save(new Food("testCanteenManager1", "descript", "nope", 0.05, 100, ingredientsInFood ));
 		savedFood2 = foodDao.save(new Food("testCanteenManager2"));
 		Map<Food, Integer> portions = new HashMap<>();
 		portions.put(savedFood1, 1);
@@ -51,10 +59,12 @@ class DefaultCanteenManagerTest {
 		foodDao.delete(savedFood1.getId());
 		foodDao.delete(savedFood2.getId());
 		orderDao.delete(savedOrder.getId());
+		
+		ingredientDao.delete(savedIngredient.getId());
 	}
 
 	@Test
-	void test() throws EntityUndeletableException {
+	void filterFoodNotInOrderTest() throws EntityUndeletableException {
 		int count = canteenManager.filterFoodNotInOrder(savedOrder.getId()).size();
 		savedOrder.getPortions().put(savedFood2, 1);
 		orderDao.insertFoods(savedOrder);
@@ -81,6 +91,38 @@ class DefaultCanteenManagerTest {
 		assertEquals(foodsInOrder + foodsNotInOrder, allFoods);
 		
 		foodDao.delete(newFood.getId());
+	}
+	@Test
+	void getAllToBuyTest() {
+	List<ShoppingListItemOverview> all = canteenManager.getAllToBuy();
+	int count = all.size();
+	boolean found = false;
+	for(ShoppingListItemOverview item : all) {
+		if(item.getIngredient().getId().equals(savedIngredient.getId())) {
+			found = true;
+			int portionsOfFood = savedOrder.getPortions().get(savedFood1);
+			int amoutNeeded = savedFood1.getIngredientsById().get(savedIngredient.getId())*portionsOfFood - savedIngredient.getAmountAvailiable();
+			assertEquals(2, amoutNeeded);
+		}
+	}
+	assertTrue(found);
+	assertEquals(1, count);
+	
+	}
+
+
+	@Test
+	void getItemsForShoppingList() {
+		LocalDateTime date = savedOrder.getDay();
+		List<ShoppingListItemOverview> items = canteenManager.getItemsForShoppingList(date);
+		assertEquals(1, items.size());
+		//TODO test
+	}
+
+	@Test
+	void getNumberOfToBuyTest() {
+		int number = canteenManager.getNumberOfToBuy();
+		assertEquals(canteenManager.getAllToBuy().size(), number);
 	}
 
 }
